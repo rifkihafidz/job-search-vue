106a9e3eb145b4954ec129f35dcac8efd49dcab7
EAD - Muhammad Rifki Hafidz <muhammad.hafidz@adira.co.id>
2023-11-29 11:03:20 +0700

    Setup tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <h1 class="mt-8 text-center text-6xl">Hello Vue</h1>
 </template>
